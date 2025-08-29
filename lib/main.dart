c62229f580767157ceda92e649bc79ed7614fa75
Yokub2144 <suwat.paomong@gmail.com>
2025-08-30 02:46:27 +0700

    up

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/login_pages.dart';
+import 'package:flutter_application_1/pages/register_pages.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +12,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Flutter Demo',
-      home: Login()
-    );
+    return const MaterialApp(title: 'Flutter Demo', home: RegisterPages());
   }
 }
---
+++ lib/pages/showlotto_pages.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class showlotto extends StatefulWidget {
+  const showlotto({super.key});
+
+  @override
+  State<showlotto> createState() => _showlottoState();
+}
+
+class _showlottoState extends State<showlotto> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
